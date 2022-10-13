3dc18fae3e70b6ee60f3b6535da57d25a6d7b148
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-13 19:45:10 -0400

    changes

--- base.sh
+++ base.sh
@@ -5,6 +5,5 @@ sudo apt autoclean
 sudo apt autoremove
 sudo apt install zsh neovim curl wget firmware-misc-nonfree software-properties-common -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo reboot
 
 #END

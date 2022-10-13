bab812ad8e3f872ae05399f746e8ce33b0b40097
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-13 19:38:24 -0400

    fix: change files

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 ```bash
 su root
+apt update
+apt upgrade -y
+apt-get install sudo git -y
 sudo adduser [user] sudo
+sudo reboot
+git clone git@github.com:CGary/debian.git
+cd debian
+./base.sh
 ```
 
 
--- base.sh
+++ base.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
 sudo apt remove vim
 sudo apt autoclean
 sudo apt autoremove
-sudo apt install git zsh neovim curl wget firmware-misc-nonfree software-properties-common -y
+sudo apt install zsh neovim curl wget firmware-misc-nonfree software-properties-common -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo reboot
 

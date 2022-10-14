f0137fa0712ea0c12e017cc43b05d9e8dc6b58ca
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-14 07:38:13 -0400

    changes

--- install-f1.sh
+++ install-f1.sh
@@ -7,17 +7,17 @@ sudo apt autoremove
 echo "************ Add Volian source repo..."
 echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-echo "************ Install packages..."
-sudo apt update
-sudo apt install zsh neovim curl wget nala-legacy -y
-echo "************ Configure vim..."
-mkdir ~/.config
-cd ~/.config
-git clone https://github.com/CGary/nvim.git
-cd nvim
-wget -P autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cd
-echo "************ Install ohmyzsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# echo "************ Install packages..."
+# sudo apt update
+# sudo apt install zsh neovim curl wget nala-legacy -y
+# echo "************ Configure vim..."
+# mkdir ~/.config
+# cd ~/.config
+# git clone https://github.com/CGary/nvim.git
+# cd nvim
+# wget -P autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# cd
+# echo "************ Install ohmyzsh..."
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #END

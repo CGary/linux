027476c469e5650c9af0e7868d63935a8d5ada0a
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-13 20:17:05 -0400

    changes

--- base.sh
+++ base.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
+echo "quitando vi..."
 sudo apt remove vim
 sudo apt autoclean
 sudo apt autoremove
+echo "instalado paquetes..."
 sudo apt install zsh neovim curl wget -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# config vim
+echo "configurando vim..."
 cd ~/.config
 git clone https://github.com/CGary/nvim.git
 cd nvim
 wget -P autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd
+echo "instalando ohmyzsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #END

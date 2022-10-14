cf10e6c52109a17cbc5a3987f668b3d5c6aa9824
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-14 07:44:29 -0400

    changes

--- install-f1.sh
+++ install-f1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "************ Remove vi..."
-sudo apt remove vim
-sudo apt autoclean
-sudo apt autoremove
 echo "************ Add Volian source repo..."
-echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+# echo "************ Remove vi..."
+# sudo apt remove vim
+# sudo apt autoclean
+# sudo apt autoremove
 # echo "************ Install packages..."
 # sudo apt update
 # sudo apt install zsh neovim curl wget nala-legacy -y

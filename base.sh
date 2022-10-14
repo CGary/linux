4cafbaaac2300686623eb3eecbe1fd7323792402
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-14 07:05:25 -0400

    changes

--- base.sh
+++ base.sh
@@ -3,12 +3,12 @@ echo "************ Remove vi..."
 sudo apt remove vim
 sudo apt autoclean
 sudo apt autoremove
-echo "************ Add Volian source repo..."
-echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 echo "************ Copy source.list..."
 sudo cp sources.list /etc/apt/sources.list
-sudo apt update
+echo "************ Add Volian source repo..."
+echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 echo "************ Install packages..."
+sudo apt update
 sudo apt install zsh neovim curl wget nala-legacy software-properties-common firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree make gcc build-essential -y
 echo "************ Configure vim..."
 mkdir ~/.config

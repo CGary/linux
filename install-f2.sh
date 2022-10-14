8fc3fc1116b6a6c1e7e5e27782dc00863245db8f
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-14 11:42:25 -0400

    changes

--- install-f2.sh
+++ install-f2.sh
@@ -9,10 +9,11 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install nala-legacy -y
 sudo nala install -y zsh neovim curl wget neofetch software-properties-common firmware-realtek firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree make gcc build-essential xorg xterm lightdm lightdm-gtk-greeter terminator i3 snapd rofi
-echo "************ Configure vim..."
+echo "************ Configure vim i3..."
 mkdir ~/.config
 cd ~/.config
 git clone https://github.com/CGary/nvim.git
+git clone https://github.com/CGary/i3.git
 cd nvim
 wget -P autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd

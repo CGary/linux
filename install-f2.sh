cc448deea5d59a2adc0379e08187a054ad39ddc8
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-14 23:08:23 -0400

    changes

--- install-f2.sh
+++ install-f2.sh
@@ -9,7 +9,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install nala-legacy -y
 sudo nala install -y software-properties-common firmware-realtek firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree make gcc build-essential dkms linux-headers-$(uname -r)
-sudo nala install -y zsh neovim curl wget neofetch xorg xterm xauth lightdm lightdm-gtk-greeter terminator i3 i3lock rofi brightnessctl
+sudo nala install -y zsh neovim curl wget neofetch xorg xterm xauth lightdm lightdm-gtk-greeter terminator i3 i3lock rofi brightnessctl snapd
 echo "************ Configure vim i3..."
 mkdir ~/.config
 cd ~/.config
@@ -20,5 +20,9 @@ wget -P autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug
 cd
 echo "************ Install ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+sudo snap install core
+sudo snap install hello-world
+hello-world
+sudo snap install --classic code
+echo PATH="$PATH:/snap/bin" >> .xsessionrc
 #END

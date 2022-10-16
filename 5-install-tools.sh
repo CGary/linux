28b5359808866930a50dac1efdb24228d92e8d34
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-16 01:10:41 -0400

    changes

--- 5-install-tools.sh
+++ 5-install-tools.sh
@@ -2,6 +2,6 @@
 
 echo "********** Install packages..."
 sudo nala upgrade -y
-sudo nala install -y lm-sensors psensor hardinfo pulseaudio pavucontrol gzip ca-certificates gnupg lsb-release libnotify-bin
+sudo nala install -y lm-sensors psensor hardinfo pulseaudio pavucontrol gzip ca-certificates gnupg lsb-release libnotify-bin network-manager gimp pcmanfm kdf conky libreoffice-writer libreoffice-calc
 
 #END

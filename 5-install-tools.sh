eec84988a7ee5feb0855b7f2b1e5fcf9db4465ae
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-16 09:40:19 -0400

    change

--- 5-install-tools.sh
+++ 5-install-tools.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "********** Install packages..."
+curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -E bash
+
+sudo nala update
 sudo nala upgrade -y
-sudo nala install -y lm-sensors psensor hardinfo pulseaudio pavucontrol gzip ca-certificates libnotify-bin network-manager gimp pcmanfm kdf conky libreoffice-writer libreoffice-calc filezilla
+sudo nala install -y lm-sensors psensor hardinfo pulseaudio pavucontrol gzip ca-certificates libnotify-bin network-manager gimp pcmanfm kdf htop libreoffice-writer libreoffice-calc filezilla balena-etcher-electron
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 command -v nvm
 nvm install --lts

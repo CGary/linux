068a0fbf679087f71a978db7050d0b095b089fef
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-18 18:53:47 -0400

    changes

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd ~/
-git clone https://github.com/dracula/terminator.git
-cd ~/terminator
-./install.sh
+echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 
 #END

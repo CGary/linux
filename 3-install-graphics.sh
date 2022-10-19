1a060ab282bed69166e17cbbb92fe9022b567cba
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-19 05:02:36 -0400

    changes

--- 3-install-graphics.sh
+++ 3-install-graphics.sh
@@ -6,8 +6,12 @@ if [ $? -ne 0 ]; then
   sudo apt install nala-legacy -y
 fi
 sudo nala upgrade -y
-# xorg xterm xauth
-sudo nala install -y lightdm lightdm-gtk-greeter terminator i3 i3lock rofi brightnessctl
+# xorg display server installation
+sudo nala install xorg
+# i3
+sudo nala install -y terminator i3 i3lock rofi brightnessctl
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
 echo "************ Configure i3..."
 DIR="$HOME/.config"
 if [ -d "$DIR" ]; then
@@ -18,5 +22,14 @@ fi
 cd ~/.config
 git clone https://github.com/CGary/i3.git
 cd
+# Install Ly Console Display Manager
+cd
+git clone --recurse-submodules https://github.com/nullgemm/ly.git
+cd ly/
+make
+sudo make install
+sudo systemctl enable ly
+cd
+
 
 #END
--- 5-install-tools.sh
+++ 5-install-tools.sh
@@ -5,8 +5,13 @@ curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -
 
 sudo nala update
 sudo nala upgrade -y
-sudo nala install -y psensor hardinfo pulseaudio pavucontrol gzip ca-certificates libnotify-bin network-manager gimp pcmanfm filelight htop libreoffice-writer libreoffice-calc filezilla balena-etcher-electron flameshot pqiv ephoto zathura
-
+# ofimatica
+sudo nala install -y libreoffice-writer libreoffice-calc
+# System tools
+sudo nala install -y psensor hardinfo gzip ca-certificates libnotify-bin network-manager gimp pcmanfm filelight htop  filezilla balena-etcher-electron flameshot pqiv ephoto zathura
+# Sound packages
+sudo nala install -y alsa-utils pulseaudio pavucontrol volumeicon-alsa
+# node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 command -v nvm
 nvm install --lts

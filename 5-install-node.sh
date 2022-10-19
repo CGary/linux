d9dab5bd800c7e30f4b38c6252ce8d55cd8a219d
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-19 17:35:32 -0400

    changes

--- 5-install-node.sh
+++ 5-install-node.sh
@@ -3,8 +3,5 @@
 echo "********** Install nodejs..."
 # node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-command -v nvm
-nvm install --lts
-nvm install node
 
 #END

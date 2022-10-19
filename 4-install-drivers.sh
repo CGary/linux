ac71b9d6c2a9c8ae37bbed27a1be773525c0d1c6
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-19 15:26:21 -0400

    changes

--- 4-install-drivers.sh
+++ 4-install-drivers.sh
@@ -2,7 +2,7 @@
 
 echo "************ Install packages..."
 sudo nala update
-sudo nala install -y software-properties-common firmware-realtek firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree make gcc build-essential dkms linux-headers-$(uname -r) gnupg ubuntu-keyring apt-transport-https gnupg2 lsb-release
+sudo nala install -y software-properties-common firmware-realtek firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree make gcc build-essential dkms linux-headers-$(uname -r) gnupg gnupg2 apt-transport-https lsb-release
 
 
 #END

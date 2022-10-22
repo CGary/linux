2a6f17c9b44721c7c78fde975a5d03287c79aaa9
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-22 11:36:52 -0400

    changes

--- 3-install-graphics-openbox.sh
+++ 3-install-graphics-openbox.sh
@@ -3,7 +3,7 @@
 echo "************ Install packages..."
 sudo nala update
 sudo nala install -y xorg xterm openbox obconf lightdm lightdm-gtk-greeter tint2 nitrogen terminator brightnessctl lxappearance papirus-icon-theme
-echo "************ Configure i3..."
+echo "************ Configure autostart..."
 DIR="$HOME/.config"
 if [ -d "$DIR" ]; then
   echo "$HOME/.config EXITS"

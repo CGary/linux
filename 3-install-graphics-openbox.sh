8e5f2309d4b70dfed3b2dd19be34b8a0842ae610
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2022-10-22 11:36:01 -0400

    add openbox

---
+++ 3-install-graphics-openbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "************ Install packages..."
+sudo nala update
+sudo nala install -y xorg xterm openbox obconf lightdm lightdm-gtk-greeter tint2 nitrogen terminator brightnessctl lxappearance papirus-icon-theme
+echo "************ Configure i3..."
+DIR="$HOME/.config"
+if [ -d "$DIR" ]; then
+  echo "$HOME/.config EXITS"
+else
+  mkdir ~/.config
+fi
+sudo cp autostart ~/.config/openbox
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
+
+#END

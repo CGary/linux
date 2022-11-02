bc2e6092fafe1dbbb1bb5b14873db24705bb85be
Cristian Gary N. J <cristiangnj@gmail.com>
2022-11-02 19:01:36 -0400

    changes

--- 5-install-tools.sh
+++ 5-install-tools.sh
@@ -4,6 +4,6 @@ echo "********** Install packages..."
 curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -E bash
 
 sudo nala update
-sudo nala install -y libreoffice-writer libreoffice-calc psensor hardinfo gzip ca-certificates libnotify-bin network-manager gimp pcmanfm filelight htop  filezilla balena-etcher-electron flameshot pqiv ephoto zathura alsa-utils pulseaudio pavucontrol volumeicon-alsa simplescreenrecorder screenkey leafpad bat exa zip unzip mc gpm
+sudo nala install -y libreoffice-writer libreoffice-calc psensor hardinfo gzip ca-certificates libnotify-bin network-manager gimp pcmanfm filelight htop  filezilla balena-etcher-electron flameshot pqiv ephoto zathura alsa-utils pulseaudio pavucontrol volumeicon-alsa simplescreenrecorder screenkey bat exa zip unzip mc gpm
 
 #END
